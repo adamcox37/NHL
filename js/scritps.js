784fba0c6c8f8f853f38c76ba33036a6ee5f9136
Adam Cox <adam_mcox@yahoo.com>
2018-10-01 20:52:16 -0400

    style changes

--- js/scritps.js
+++ js/scritps.js
@@ -1,1 +1,24 @@
-// JS
+// EASTERN CONFERENCE
+
+var east = [
+  "Hurricanes",
+  "Blue Jackets",
+  "Devils",
+  "Islanders",
+  "Rangers",
+  "Penguins",
+  "Flyers",
+  "Capitals",
+  "Bruins",
+  "Sabres",
+  "Red Wings",
+  "Panthers",
+  "Canadians",
+  "Senators",
+  "Lightings",
+  "Maple Leafs"
+];
+
+var randomEast() = myArray[Math.floor(Math.random()*east.length)];
+
+ddocument.getElementById("eastPick").innerHTML = x;
