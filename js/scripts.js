4ff0e825207df73d6cc72aa601d070adbce85ac4
Adam Cox <adam_mcox@yahoo.com>
2018-10-15 20:40:53 -0400

    revamping javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 // EASTERN CONFERENCE
+document.getElementById("east").addEventListener("click", getEast);
+
 function getEast() {
-	var east = [
+	var x = [
 		'Carolina Hurricanes', 
 		'Columbus Blue Jackets', 
 		'New Jersey Devils', 
@@ -18,17 +20,18 @@ function getEast() {
 		'Tampa Bay Lighting',
 		'Toronto Maple Leafs'
 	];
-	var i;
 
-	for (i = 0; i < east.length; i++) {
-		var eastWinner = east[Math.floor(Math.random() * east.length)];
-		document.getElementById('eastPick').innerHTML = eastWinner
-	}
-};
+	var rand = x[Math.floor(Math.random() * x.length)];
+
+	document.getElementById("east").innerHTML = rand;
+
+}
 
 // WESTERN CONFERENCE
+document.getElementById("west").addEventListener("click", getWest);
+
 function getWest() {
-	var west = [
+	var y = [
 		'Chicago Blackhawks',
 		'Colorado Avalanche',
 		'Dallas Stars',
@@ -45,54 +48,24 @@ function getWest() {
 		'Vancouver Canucks',
 		'Vegas Golden Knights'
 	];
-	var i;
 
-	for (i = 0; i < west.length; i++) {
-		var westWinner = west[Math.floor(Math.random() * west.length)];
-		document.getElementById('westPick').innerHTML = westWinner
-	}
-};
+	var rand = y[Math.floor(Math.random() * y.length)];
+
+	document.getElementById("west").innerHTML = rand;
+}
 
 
 // STANLEY CUP
-function getChamp() {
-	var nhl = [
-		'Carolina Hurricanes', 
-		'Columbus Blue Jackets', 
-		'New Jersey Devils', 
-		'New York Islanders', 
-		'New York Rangers', 
-		'Philadelphia Flyers', 
-		'Pittsburg Penguins', 
-		'Washington Capitals',
-		'Boston Bruins',
-		'Buffalo Sabres',
-		'Detroit Red Wings',
-		'Florida Panthers',
-		'Montreal Canadiens',
-		'Ottawa Senators',
-		'Tampa Bay Lighting',
-		'Toronto Maple Leafs',
-		'Chicago Blackhawks',
-		'Colorado Avalanche',
-		'Dallas Stars',
-		'Minnesota Wild',
-		'Nashville Predators',
-		'St Louis Blues',
-		'Winnipeg Jets',
-		'Anaheim Ducks',
-		'Arizona Coyotes',
-		'Calgary Flames',
-		'Edmondton Oilers',
-		'Los Angeles Kings',
-		'San Jose Sharks',
-		'Vancouver Canucks',
-		'Vegas Golden Knights'
-		];
-	var i;
+document.getElementById("champ").addEventListener("click", getChamp);
 
-	for (i = 0; i < nhl.length; i++) {
-		var cupWinner = nhl[Math.floor(Math.random() * nhl.length)];
-		document.getElementById('randyPick').innerHTML = cupWinner
-	}
-};
+function getChamp(){}
+
+
+
+
+
+
+
+
+
+
